f5735fe4a4ac9d148a3e0bd3fec9029ffa54ec4e
Adam Fidel <refuse@gmail.com>
2015-09-01 23:25:27 -0500

    Adding link.

--- 229e/229e.cc
+++ 229e/229e.cc
@@ -1,3 +1,6 @@
+// Solution to challenge #229 easy
+// https://www.reddit.com/r/dailyprogrammer/comments/3i99w8/20150824_challenge_229_easy_the_dottie_number/
+
 #include <iostream>
 #include <cmath>
 #include <Stream.h>
