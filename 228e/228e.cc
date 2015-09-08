d15cd5a66014ade44e80589c34d7e404e464ec0d
Adam Fidel <refuse@gmail.com>
2015-09-08 17:37:47 -0500

    Sorted words.

---
+++ 228e/228e.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class line : public std::string {};
+
+std::istream &operator>>(std::istream &is, line &l)
+{
+    std::getline(is, l);
+    return is;
+}
+
+int main(int argc, char* argv[])
+{
+  std::for_each(
+    std::istream_iterator<line>(std::cin),
+    std::istream_iterator<line>(),
+    [](auto&& str) {
+      bool forward = std::is_sorted(str.begin(), str.end(), std::less<char>());
+      bool backward = std::is_sorted(str.begin(), str.end(), std::greater<char>());
+
+      const std::string result = forward ? "IN ORDER" : backward ? "REVERSE ORDER" : "NOT IN ORDER";
+
+      std::cout << str << " " << result << std::endl;
+  });
+
+  return 0;
+}
